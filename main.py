dc566df10f7a4743188535a27b7a7f27adf02cd2
PawWitGit <git.pawelwitala@gmail.com>
2021-07-28 14:23:37 +0200

    basic functions OK

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ import matplotlib as plt
 import matplotlib.pyplot as plt
 import numpy as np
 
+from read_data import ReadData
 
+"""Create ReadData objects"""
 
-data = pd.read_csv("CH4_Flux_BigTrail_Goldstream_AK.csv")
-print("\n")
-print(data.head())
+read_data = ReadData("CH4_Flux_BigTrail_Goldstream_AK.csv")
+
+"""Run functions"""
+read_data.return_data_file()
+read_data.return_data_table()
+
--- read_data.py
+++ read_data.py
@@ -1,6 +1,19 @@
+import pandas as pd
+import matplotlib as plt
+import matplotlib.pyplot as plt
+import numpy as np
+
 class ReadData():
+    import pandas as pd
+
     def __init__(self, lake_methan_history):
         self.lake_methan_history  = lake_methan_history
 
-    def return_data(self):
-        print(self.lake_methan_history)    
+    def return_data_file(self):
+        print("You read: ", self.lake_methan_history, "\n")
+        
+
+    def return_data_table(self):
+        data_info = pd.read_csv(self.lake_methan_history)
+        print(data_info)
+        return data_info
